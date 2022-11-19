ca36fd5182428a00a00cab90d5f497fb1c9c29cf
Jairo Miranda <114257578+Jairo-mirand@users.noreply.github.com>
2022-11-18 20:32:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ db()
 app.use(express.json())
 app.use('/api',require('./routes/api'))
 app.use('/api/items',require('./routes/items'))
+app.use('/api/generos',require('./routes/generos'))
+app.use('/api/actores',require('./routes/actores'))
 app.listen(puerto,()=>{
     console.log("servidor activo en el puerto: "+ puerto)
 })
